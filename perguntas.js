1dbafcad3e2096a3f65c7cdca15029ebcf171b6f
Anajubaluba <00001087542777sp@al.educacao.sp.gov.br>
2024-11-29 19:15:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Raciocínio Logico',
+    'O que só aumenta e nunca diminui?',
+    'A idade! Não dá pra ir ficando mais novo com o passar do tempo'
+)
+
+criaCartao(
+    'Raciocínio Logico',
+    'Raul foi sozinho até a padaria no centro da cidade. Durante o percurso, encontrou dois garotos passeando com três gatos, que estavam brincando com dois patos, que, por sua vez, tinham dois donos. Quantos seres no total foram com Raul até a padaria?',
+    'Zero. Porque ele só encontrou os outros na rua, mas no texto disse que ele foi até a padaria sozinho.'
+)
